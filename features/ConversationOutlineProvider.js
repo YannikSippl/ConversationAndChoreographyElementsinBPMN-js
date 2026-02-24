fe256900b9042fde1b48943ed4eb0e7b27788745
YannikSippl <yannik@sippl.de>
2026-02-24 11:25:39 +0100

    added ConversationOutlineProvider to get precise outlines for participants and conversation nodes and not standard round ones

---
+++ features/ConversationOutlineProvider.js
@@ -0,0 +1,62 @@
+import { isAny } from 'bpmn-js/lib/util/ModelUtil';
+import { create as svgCreate, attr as svgAttr } from 'tiny-svg';
+
+const OFFSET = 5;
+
+export default function ConversationOutlineProvider(outline, styles) {
+  this._styles = styles;
+  outline.registerProvider(2000, this); // > default(1000)
+}
+
+ConversationOutlineProvider.$inject = [ 'outline', 'styles' ];
+
+ConversationOutlineProvider.prototype.getOutline = function(element) {
+  const OUTLINE_STYLE = this._styles.cls('djs-outline', [ 'no-fill' ]);
+
+  if (isAny(element, [ 'conversation:ConversationNode' ])) {
+    const outline = svgCreate('polygon');
+    svgAttr(outline, { points: getHexPoints(element.width, element.height, OFFSET), ...OUTLINE_STYLE });
+    return outline;
+  }
+
+  if (isAny(element, [ 'conversation:Participant' ])) {
+    const outline = svgCreate('rect');
+    svgAttr(outline, {
+      x: -OFFSET, y: -OFFSET,
+      width: element.width + OFFSET * 2,
+      height: element.height + OFFSET * 2,
+      rx: 0,
+      ...OUTLINE_STYLE
+    });
+    return outline;
+  }
+};
+
+ConversationOutlineProvider.prototype.updateOutline = function(element, outline) {
+  if (isAny(element, [ 'conversation:ConversationNode' ]) && outline.tagName === 'polygon') {
+    svgAttr(outline, { points: getHexPoints(element.width, element.height, OFFSET) });
+    return true;
+  }
+
+  if (isAny(element, [ 'conversation:Participant' ]) && outline.tagName === 'rect') {
+    svgAttr(outline, {
+      x: -OFFSET, y: -OFFSET,
+      width: element.width + OFFSET * 2,
+      height: element.height + OFFSET * 2
+    });
+    return true;
+  }
+
+  return false;
+};
+
+function getHexPoints(width, height, o = 0) {
+  return [
+    `${width * 0.25 - o},${-o}`,
+    `${width * 0.75 + o},${-o}`,
+    `${width + o},${height / 2}`,
+    `${width * 0.75 + o},${height + o}`,
+    `${width * 0.25 - o},${height + o}`,
+    `${-o},${height / 2}`
+  ].join(' ');
+}
--- features/index.js
+++ features/index.js
@@ -3,12 +3,14 @@ import ConversationPalette from './ConversationPalette';
 import ConversationRules from './ConversationRules';
 import ConversationCanConnectPatch from './ConversationCanConnectPatch';
 import ConversationInteractionEvents from './ConversationInteractionEvents';
+import ConversationOutlineProvider from './ConversationOutlineProvider';
 
 export default {
-    __init__: ['conversationRenderer', 'conversationPalette', 'conversationRules', 'conversationCanConnectPatch', 'conversationInteractionEvents'],
+    __init__: ['conversationRenderer', 'conversationPalette', 'conversationRules', 'conversationCanConnectPatch', 'conversationInteractionEvents', 'conversationOutlineProvider'],
     conversationRenderer: ['type', ConversationRenderer],
     conversationPalette: ['type', ConversationPalette],
     conversationRules: ['type', ConversationRules],
     conversationCanConnectPatch: ['type', ConversationCanConnectPatch],
-    conversationInteractionEvents: ['type', ConversationInteractionEvents]
+    conversationInteractionEvents: ['type', ConversationInteractionEvents],
+    conversationOutlineProvider: ['type', ConversationOutlineProvider]
 };
