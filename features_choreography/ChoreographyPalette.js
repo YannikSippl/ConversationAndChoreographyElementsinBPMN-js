641a691b88216615a320f7f8252d4365ed8dd131
Yannik Sippl <yannik@sippl.de>
2026-03-08 13:22:18 +0100

    added custom entries for initator and response message so they can be created from the palette

--- features_choreography/ChoreographyPalette.js
+++ features_choreography/ChoreographyPalette.js
@@ -15,15 +15,42 @@ export default class ChoreographyPalette {
             const shape = elementFactory.createShape({ type: 'choreography:ChoreographyTask' });
             create.start(event, shape);
         }
+        function createInitiatorMessage(event){
+            const shape = elementFactory.createShape({ type: 'choreography:InitiatorMessage' });
+            create.start(event, shape);
+        }
+
+        function createResponseMessage(event){
+            const shape = elementFactory.createShape({ type: 'choreography:ResponseMessage' });
+            create.start(event, shape);
+        }
 
         return {
             'create.choreography-task': {
                 group: 'choreography',
-                className: 'bpmn-icon-task',
+                className: 'bpmn-icon-lane-divide-three',
                 title: translate('Create Choreography-Task'),
                 action: {
                     dragstart: createChoreographyTask,
                     click: createChoreographyTask
+                }
+            },
+            'create.initiatingMessage': {
+                group: 'choreography',
+                className: 'bpmn-icon-start-event-message',
+                title: translate('Create initiating Message'),
+                action: {
+                    dragstart: createInitiatorMessage,
+                    click: createInitiatorMessage
+                }
+            },
+             'create.responseMessage': {
+                 group: 'choreography',
+                className: 'bpmn-icon-end-event-message',
+                title: translate('Create response Message'),
+                action: {
+                    dragstart: createResponseMessage,
+                    click: createResponseMessage
                 }
             }
         };
