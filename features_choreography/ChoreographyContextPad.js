7c7b32429141d8d67dd5dd80d5c82fa3228105a1
Yannik Sippl <yannik@sippl.de>
2026-03-08 12:01:23 +0100

    added custom contex pad to allow adding a responder to a choreography task

---
+++ features_choreography/ChoreographyContextPad.js
@@ -0,0 +1,56 @@
+import { is } from 'bpmn-js/lib/util/ModelUtil';
+
+export default class ChoreographyContextPad {
+    constructor(contextPad, bpmnFactory, modeling, translate) {
+        this.bpmnFactory = bpmnFactory;
+        this.modeling = modeling;
+        this.translate = translate;
+
+        contextPad.registerProvider(this);
+    }
+
+    getContextPadEntries(element) {
+        if (!is(element, 'choreography:ChoreographyTask')) {
+            return {};
+        }
+
+        const { bpmnFactory, modeling, translate } = this;
+
+        function addResponder(event, taskElement) {
+            const taskBO = taskElement.businessObject;
+            const parentBO = taskElement.parent && taskElement.parent.businessObject;
+
+            if (!parentBO) {
+                return;
+            }
+
+            const currentResponders = taskBO.participantRef || [];
+            const responder = bpmnFactory.create('choreography:Responder', {
+                name: `Responder ${currentResponders.length + 1}`
+            });
+
+            // Update global list of participants on process level.
+            modeling.updateModdleProperties(taskElement, parentBO, {
+                participants: [ ...(parentBO.participants || []), responder ]
+            });
+
+            // Reference responder from the choreography task.
+            modeling.updateModdleProperties(taskElement, taskBO, {
+                participantRef: [ ...currentResponders, responder ]
+            });
+        }
+
+        return {
+            'append.responder': {
+                group: 'model',
+                className: 'bpmn-icon-participant',
+                title: translate('Append Responder'),
+                action: {
+                    click: addResponder
+                }
+            }
+        };
+    }
+}
+
+ChoreographyContextPad.$inject = [ 'contextPad', 'bpmnFactory', 'modeling', 'translate' ];
--- features_choreography/index.js
+++ features_choreography/index.js
@@ -1,15 +1,18 @@
 import ChoreographyRenderer from "./ChoreographyRenderer";
 import ChoreographyRules from "./ChoreographyRules";
 import ChoreographyNameEditing from "./ChoreographyNameEditing";
+import ChoreographyContexPad from "./ChoreographyContextPad";
 
 
 export default {
     __init__: [
         'choreographyRenderer',
         'choreographyRules',
-        'choreographyNameEditing'
+        'choreographyNameEditing',
+        'choreographyContextPad'
     ],
     choreographyRenderer: ['type', ChoreographyRenderer],
     choreographyRules: ['type', ChoreographyRules],
-    choreographyNameEditing: ['type', ChoreographyNameEditing]
+    choreographyNameEditing: ['type', ChoreographyNameEditing],
+    choreographyContextPad: ['type', ChoreographyContexPad]
 }
